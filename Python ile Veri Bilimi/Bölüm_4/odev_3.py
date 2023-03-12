e3755d36b57e608fe1c2687a323baf915dd76cae
Enes Fehmi Manan <88631980+enesmanan@users.noreply.github.com>
2023-03-12 13:36:43 +0300

    Add files via upload

---
+++ Python ile Veri Bilimi/Bölüm_4/odev_3.py
@@ -0,0 +1,15 @@
+ulkeler = ['Turkiye', 'Ispanya', 'Cin', 'Italya', 'ABD', 'Ekvador', 'Fas', 'Malta']
+
+for i in ulkeler:
+    if len(i) > 5:
+        print(i)
+        
+print('---------------------------------------------')
+
+for j in ulkeler:
+    if len(j) < 5:
+        j = j.replace(j,'*')
+        print(j)
+    else:
+        pass
+        print(j)
